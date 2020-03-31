88081434dd483153c03bb0166f3eb0cf00defd1a
Evgenii Kuzmichev <evgenii.e.kuzmichev@gmail.com>
2020-03-31 15:33:52 +0300

    Exercise 1.3: Add PrintableSyntax

--- src/main/scala/ru/ekuzmichev/`1.3 Printable Library`.scala
+++ src/main/scala/ru/ekuzmichev/`1.3 Printable Library`.scala
@@ -13,13 +13,34 @@ object `1.3 Printable Library` {
   object PrintableInstances {
     implicit val stringPrintable: Printable[String] = (s: String) => s
     implicit val intPrintable: Printable[Int]       = (i: Int) => i.toString
+    implicit val catPrintable: Printable[Cat]       = (cat: Cat) => s"${cat.name} is a ${cat.age} years-old ${cat.color} cat"
   }
+
+  object PrintableSyntax {
+    implicit class PrintableOps[A](value: A) {
+      def format(implicit printable: Printable[A]): String = printable.format(value)
+      def print(implicit printable: Printable[A]): Unit    = println(value.format)
+    }
+  }
+
+  case class Cat(name: String, age: Int, color: String)
+
 }
 
 object Test extends App {
   import `1.3 Printable Library`._
   import PrintableInstances._
+  import PrintableSyntax._
 
-  Printable.print("Hello")
-  Printable.print(1)
+  val string = "Hello"
+  Printable.print(string)
+  string.print
+
+  val int = 42
+  Printable.print(int)
+  int.print
+
+  val tom = Cat("Tom", 11, "grey")
+  Printable.print(tom)
+  tom.print
 }
