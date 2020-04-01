a21e8d7a05a5c8e5ce14c32660cccf7554a4f776
Evgenii Kuzmichev <evgenii.e.kuzmichev@gmail.com>
2020-04-01 21:12:04 +0300

    Add `3.7 Contravariant and Invariant in Cats`

---
+++ src/main/scala/ru/ekuzmichev/chapter3/`3.7 Contravariant and Invariant in Cats`.scala
@@ -0,0 +1,27 @@
+package ru.ekuzmichev.chapter3
+
+object `3.7 Contravariant and Invariant in Cats` {}
+
+object ContravariantAndInvariantInCatsTest extends App {
+  import cats.instances.string._
+  import cats.{ Contravariant, Show }
+
+  val showString = Show[String]
+
+  val showSymbol: Show[Symbol] = Contravariant[Show].contramap(showString)((sym: Symbol) => s"'${sym.name}")
+
+  import cats.syntax.contravariant._
+  val showInt: Show[Int] = showString.contramap(_.toString)
+
+  println(showSymbol.show(Symbol("dave")))
+  println(showInt.show(42))
+
+  import cats.Monoid
+  import cats.instances.string._
+  import cats.syntax.invariant._
+  import cats.syntax.semigroup._
+
+  implicit val symbolMonoid: Monoid[Symbol] = Monoid[String].imap(Symbol(_))(_.name)
+  println(Monoid[Symbol].empty)
+  println(Symbol("a") |+| Symbol("few") |+| Symbol("words"))
+}
