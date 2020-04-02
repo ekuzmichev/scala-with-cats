8020b46a6d87e278b28b4b8c48d6d6c9f82effd1
Evgenii Kuzmichev <evgenii.e.kuzmichev@gmail.com>
2020-04-02 16:20:02 +0300

    Add `4.1.2 Exercise: Ge􏰂ng Func-y`

---
+++ src/main/scala/chapter4/`4.1.2 Exercise: Ge􏰂ng Func-y`.scala
@@ -0,0 +1,13 @@
+package chapter4
+
+import scala.language.higherKinds
+
+object `4.1.2 Exercise: Ge􏰂ng Func-y` {
+  trait Monad[F[_]] {
+    def pure[A](a: A): F[A]
+
+    def flatMap[A, B](value: F[A])(f: A => F[B]): F[B]
+
+    def map[A, B](value: F[A])(f: A => B): F[B] = flatMap(value)(a => pure(f(a)))
+  }
+}
