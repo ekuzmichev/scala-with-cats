b2ab49d6a6e3bec49c326d71c340ead04e4001f2
Evgenii Kuzmichev <evgenii.e.kuzmichev@gmail.com>
2020-04-02 15:24:07 +0300

    Add `3.8 Aside: Par􏰀tial Unifica􏰀tion`

---
+++ src/main/scala/ru/ekuzmichev/chapter3/`3.8 Aside: Par􏰀tial Unifica􏰀tion`.scala
@@ -0,0 +1,14 @@
+package ru.ekuzmichev.chapter3
+
+object `3.8 Aside: Par􏰀tial Unifica􏰀tion` {}
+
+object PartialUnificationTest extends App {
+  import cats.instances.function._
+  import cats.syntax.functor._
+
+  val func1 = (x: Int) => x.toDouble
+  val func2 = (y: Double) => y * 2
+  val func3 = func1.map(func2)
+
+  println(func3(1))
+}
