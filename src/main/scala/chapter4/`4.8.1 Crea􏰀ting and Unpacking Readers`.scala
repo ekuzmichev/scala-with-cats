990cd426328cfe027fcda53d95dfb7ad0f3982b0
Evgenii Kuzmichev <evgenii.e.kuzmichev@gmail.com>
2020-04-03 17:36:32 +0300

    Add more solutions

---
+++ src/main/scala/chapter4/`4.8.1 Crea􏰀ting and Unpacking Readers`.scala
@@ -0,0 +1,23 @@
+package chapter4
+
+object `4.8.1 Crea􏰀ting and Unpacking Readers` {
+  import cats.data.Reader
+
+  case class Cat(name: String, favoriteFood: String)
+
+  val catName: Reader[Cat, String]    = Reader(_.name)
+  val greetKitty: Reader[Cat, String] = catName.map(name => s"Hello $name")
+  val feedKitty: Reader[Cat, String]  = Reader(cat => s"Have a nice bowl of ${cat.favoriteFood}")
+  val greetAndFeed: Reader[Cat, String] =
+    for {
+      greet <- greetKitty
+      feed  <- feedKitty
+    } yield s"$greet. $feed."
+}
+
+object CreatingAndUnpackingReadersTest extends App {
+  import `4.8.1 Crea􏰀ting and Unpacking Readers`._
+
+  println(catName.run(Cat("Tom", "meat")))
+  println(greetAndFeed.run(Cat("Tom", "meat")))
+}
---
+++ src/main/scala/chapter4/`4.8.3 Exercise: Hacking on Readers`.scala
@@ -0,0 +1,32 @@
+package chapter4
+
+object `4.8.3 Exercise: Hacking on Readers` {
+  import cats.data.Reader
+  import cats.syntax.applicative._
+
+  final case class Db(
+    usernames: Map[Int, String],
+    passwords: Map[String, String]
+  )
+
+  type DbReader[A] = Reader[Db, A]
+
+  def findUsername(userId: Int): DbReader[Option[String]] = Reader(_.usernames.get(userId))
+  def checkPassword(username: String, password: String): DbReader[Boolean] =
+    Reader(db => db.passwords.get(username).contains(password))
+  def checkLogin(userId: Int, password: String): DbReader[Boolean] =
+    for {
+      username   <- findUsername(userId)
+      passwordOk <- username.map(checkPassword(_, password)).getOrElse(false.pure[DbReader])
+    } yield passwordOk
+}
+
+object HackingOnReadersTest extends App {
+  import `4.8.3 Exercise: Hacking on Readers`._
+
+  val users     = Map(1 -> "dade", 2 -> "kate", 3 -> "margo")
+  val passwords = Map("dade" -> "zerocool", "kate" -> "acidburn", "margo" -> "secret")
+  val db        = Db(users, passwords)
+  assert(checkLogin(1, "zerocool").run(db))
+  assert(!checkLogin(4, "davinci").run(db))
+}
