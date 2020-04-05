74d4b4ef5cd0d9ba99045bf4a6d18fe72b954f4f
Evgenii Kuzmichev <evgenii.e.kuzmichev@gmail.com>
2020-04-05 18:09:46 +0300

    Add `4.10 Defining Custom Monads`

---
+++ src/main/scala/chapter4/`4.10 Defining Custom Monads`.scala
@@ -0,0 +1,28 @@
+package chapter4
+
+object `4.10 Defining Custom Monads` {
+  import cats.Monad
+
+  import scala.annotation.tailrec
+
+  implicit val optionMonad: Monad[Option] = new Monad[Option] {
+    override def pure[A](x: A): Option[A] = Some(x)
+
+    override def flatMap[A, B](fa: Option[A])(f: A => Option[B]): Option[B] = fa.flatMap(f)
+
+    @tailrec
+    override def tailRecM[A, B](a: A)(f: A => Option[Either[A, B]]): Option[B] = f(a) match {
+      case Some(Right(value)) => Some(value)
+      case Some(Left(value))  => tailRecM(value)(f)
+      case None               => None
+    }
+  }
+}
+
+object DefiningCustomMonadsTest extends App {
+  import `4.10 Defining Custom Monads`._
+  import cats.Monad
+  import cats.syntax.option._
+
+  println(Monad[Option].flatMap(42.some)(x => 21.some.map(_ + x)))
+}
